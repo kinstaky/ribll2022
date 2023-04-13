84484972cae71e13a0bd7d82c7f2a81c7c1608d7
kinstaky <kinstaky@163.com>
2023-04-13 12:03:51 +0800

    add t0d3 normalize

--- src/detector/t0d3.cpp
+++ src/detector/t0d3.cpp
@@ -3,7 +3,7 @@
 namespace ribll {
 
 // center of t0d3, in mm
-const ROOT::Math::XYZVector t0d3_center{0.0, 0.0, 12.0};
+const ROOT::Math::XYZVector t0d3_center{0.0, 0.0, 130.0};
 // x range of t0d3, in mm
 const std::pair<double, double> t0d3_x_range{-32.0, 32.0};
 // y range of t0d3, in mm
@@ -12,6 +12,31 @@ const std::pair<double, double> t0d3_y_range{-32.0, 32.0};
 
 T0d3::T0d3(unsigned int run, const std::string &tag)
 : Dssd(run, "t0d3", tag) {
+
+	center_ = t0d3_center;
+	x_range_ = t0d3_x_range;
+	y_range_ = t0d3_y_range;
+}
+
+
+int T0d3::NormalizeSides(TChain *chain, bool iteration) {
+	if (SideNormalize(chain, 0, 20, iteration)) {
+		std::cerr << "Error: Normalize first side failed.\n";
+		return -1;
+	}
+	if (SideNormalize(chain, 1, 13, iteration)) {
+		std::cerr << "Error: Normalize second side failed.\n";
+		return -1;
+	}
+	return 0;
+}
+
+
+bool T0d3::NormEnergyCheck(
+	size_t,
+	const DssdFundamentalEvent&
+) const {
+	return true;
 }
 
 // bool T0D3::NormalizeFrontEnergyCheck
